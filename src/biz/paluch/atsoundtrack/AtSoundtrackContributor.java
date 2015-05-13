08c8071a18a84a371a1f686cf9a09c4089fcdb70
Mark Paluch <mpaluch@paluch.biz>
2015-05-13 22:27:31 +0200

    Use code completion in multiple places and compatibility with PyCharm, RubyMine, PhpStorm

--- src/biz/paluch/atsoundtrack/AtSoundtrackContributor.java
+++ src/biz/paluch/atsoundtrack/AtSoundtrackContributor.java
@@ -1,11 +1,15 @@
 package biz.paluch.atsoundtrack;
 
-import org.jetbrains.annotations.NotNull;
+import static com.intellij.openapi.util.text.StringUtil.*;
 
 import com.intellij.codeInsight.completion.CompletionContributor;
 import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionResultSet;
-import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.psi.PsiComment;
+import com.intellij.psi.PsiDocCommentBase;
+import com.intellij.psi.xml.XmlComment;
+import com.intellij.psi.xml.XmlToken;
 
 /**
  * @author <a href="mailto:mpaluch@paluch.biz">Mark Paluch</a>
@@ -16,16 +20,37 @@ public class AtSoundtrackContributor extends CompletionContributor {
     @Override
     public void fillCompletionVariants(CompletionParameters parameters, CompletionResultSet result) {
 
+        String prefix = parameters.getOriginalPosition().getText();
 
-        if (AtSoundtrack.getName() != null) {
-            result.addElement(new LookupElement() {
-                @NotNull
-                @Override
-                public String getLookupString() {
-                    return "@soundtrack " + AtSoundtrack.getName();
+        if (!prefix.contains("soundtrack")) {
+
+            if (!isEmpty(AtSoundtrack.getName())) {
+
+                boolean qualified = false;
+                if (parameters.getOriginalPosition() instanceof PsiComment
+                        || parameters.getOriginalPosition().getContext() instanceof PsiDocCommentBase
+                        || parameters.getOriginalPosition().getClass().getName().contains("PsiDocToken")) {
+
+                    qualified = true;
                 }
-            });
+                if (parameters.getOriginalPosition() instanceof XmlToken) {
+                    XmlToken xmlToken = (XmlToken) parameters.getOriginalPosition();
+
+                    if (xmlToken instanceof XmlComment || xmlToken.getContext() instanceof XmlComment) {
+                        qualified = true;
+                    }
+                }
+
+                if (qualified) {
+                    if (!prefix.contains("@")) {
+                        result.addElement(LookupElementBuilder.create("@soundtrack " + AtSoundtrack.getName()));
+                    } else {
+                        result.addElement(LookupElementBuilder.create("soundtrack " + AtSoundtrack.getName()));
+                    }
+                }
+            }
         }
+
         super.fillCompletionVariants(parameters, result);
     }
 }
